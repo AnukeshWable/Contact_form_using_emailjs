76806cfcce8c620ab2c0b00ea35a410d4b0ab0cf
Anukesh Wable <106083762+AnukeshWable@users.noreply.github.com>
2023-09-03 11:25:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function sendmessage() {
+    (function () {
+      emailjs.init("0beN_UUbuE7xehNY7");
+    })();
+
+    var serviceID = "service_3lfr1h8";
+    var templateID = "template_iz8gghu";
+
+    var params = {
+      sendername: document.querySelector("#name").value,
+      senderemail: document.querySelector("#email").value,
+      subject: document.querySelector("#subject").value,
+      message: document.querySelector("#message").value,
+    };
+    emailjs.send(serviceID, templateID, params).then((res) => {
+      alert(
+        "Thank you," + params["sendername"] + "! Your message has been sent"
+      );
+    });
+  }
